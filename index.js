396eb772ac69d0a196bf3e307ac2f8f3bb684ab7
Topias Kinnunen <topiasxd@gmail.com>
2025-04-09 14:08:35 +0300

    cors fixes

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import 'dotenv/config';
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJsdoc from 'swagger-jsdoc';
-import cors from "cors";
-import cookieParser from "cookie-parser";
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import users from './routes/users.js';
 import items from './routes/items.js';
@@ -25,7 +25,7 @@ const options = {
 const swaggerSpec = swaggerJsdoc(options);
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true}))
+app.use(cors({ origin: 'https://localhost:5173', credentials: true }));
 app.use(cookieParser());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
